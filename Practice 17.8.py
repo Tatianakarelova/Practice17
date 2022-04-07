fde8abb0c90a8b0515476c8eedaebbd33d6b7860
Tatianakarelova <andrey.333.k@yandex.ru>
2022-04-07 22:00:02 +0300

    практика 17,8 (вторая часть)

--- Practice 17.8.py
+++ Practice 17.8.py
@@ -14,4 +14,21 @@ for i in range(len(array)):
 
 print(array)
 
+element =(5)
+def binary_n(array, element, left, right):
+    if left > right:
+        return False
+
+    middle = (right + left) // 2
+    if array[middle] == element:
+        return middle
+    elif element < array[middle]:
+        return binary_n(array, element, left, middle - 1)
+    else:
+        return binary_n(array, element, middle + 1, right)
+
+array = [i for i in range(0, 10)]
+
+print(element,"Позиция элемента")
+
 
